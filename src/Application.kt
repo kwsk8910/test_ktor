6fc18880993e694790085904651f30a1bcc5d71b
Kawasaki Hirotsugu <kawa.ikkyou@gmail.com>
2021-01-15 22:04:00 +0900

    show hello world

--- src/Application.kt
+++ src/Application.kt
@@ -3,8 +3,23 @@ package com.example
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 
-fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+fun main(args: Array<String>) {
+    val server = embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello World",)
+            }
+            get("/demo") {
+                call.respondText("HELLO WORLD!")
+            }
+        }
+    }
+    server.start(wait = true)
+}
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
